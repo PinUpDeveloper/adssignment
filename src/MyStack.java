825fff26fbcb62f4934a8dffb990a38daae08275
Marsel <marselflexov@gmail.com>
2025-04-13 20:36:42 +0500

    mystack

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,2 +1,27 @@
-public class MyStack {
+public class MyStack<T> {
+    private MyArrayList<T> list = new MyArrayList<>();
+
+    public void push(T item) {
+        list.add(item);
+    }
+
+    public T pop() {
+        if (list.size() == 0) throw new IllegalStateException("Stack is empty");
+        T item = list.get(list.size() - 1);
+        list.remove(list.size() - 1);
+        return item;
+    }
+
+    public T peek() {
+        if (list.size() == 0) throw new IllegalStateException("Stack is empty");
+        return list.get(list.size() - 1);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
 }
