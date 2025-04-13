1677bfcef247f407544b98f9821dbb58deb42dfd
Marsel <marselflexov@gmail.com>
2025-04-13 20:53:24 +0500

    myqueue

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,2 +1,27 @@
-public class MyQueue {
+public class MyQueue<T> {
+    private MyLinkedList<T> list = new MyLinkedList<>();
+
+    public void enqueue(T item) {
+        list.add(item);
+    }
+
+    public T dequeue() {
+        if (list.size() == 0) throw new IllegalStateException("Queue is empty");
+        T item = list.get(0);
+        list.remove(0);
+        return item;
+    }
+
+    public T peek() {
+        if (list.size() == 0) throw new IllegalStateException("Queue is empty");
+        return list.get(0);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
 }
